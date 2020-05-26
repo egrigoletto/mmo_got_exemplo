00fd007b38647e1e36855b8ac29c38ecfa10b2ba
Eduardo Grigoletto <eduardo.grigoletto@itlean.com.br>
2020-05-26 11:59:53 -0300

    feat: add mongoDB connection support

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 /* importar as configurações do servidor */
 var app = require('./config/server');
+var db = require('./config/dbConnection')
 
-/* parametrizar a porta de escuta */
-app.listen(80, function(){
-	console.log('Servidor online');
-})
+
+db.connect(function (err) {
+	if (err) {
+		console.error('Erro ao conectar no mongoDB');
+		process.exit(1);
+	} else {
+		console.log('MongoDB Conectado');
+		/* parametrizar a porta de escuta */
+		app.listen(80, function () {
+			console.log('Servidor online');
+		})
+	}
+});
